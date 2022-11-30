17948ca7e7eba66b6f6894d2e630c8d125ad5ff3
msg82mx <msg82mx@gmail.com>
2022-11-29 22:05:16 -0600

    Se creo app web

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,25 @@ $('#botonEnviar').click(function(){
 
 
  }) //fin boton guardar
+
+
+
+ jQuery(document).ready(function($){
+   $('.owl-carousel').owlCarousel({
+     loop:true,
+     margin:10,
+     nav:true,
+     responsive:{
+       0:{
+         items:1
+       },
+       600:{
+         items:3
+       },
+       1000:{
+         items:3
+       }
+     }
+   })
+ })
+
